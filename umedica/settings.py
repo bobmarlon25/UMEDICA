8d0ed8ccfba82540968e25644c8c97942876271c
bobmarlon25 <julianpuentes25@gmail.com>
2024-05-21 22:05:50 -0500

    diploy 13

--- umedica/settings.py
+++ umedica/settings.py
@@ -154,3 +154,4 @@ MEDIA_URL='/media/'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
